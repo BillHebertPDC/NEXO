53a7482e62ce39de9b059c38040cd6e2bb4a420d
richh-brreto <richard.barreto@sptech.school>
2025-08-26 14:16:46 -0300

    fix (WebDataViz): rotas create MySql - corrigido as rotas para create no banco de dados - excluido arquivos de controllers que não estavam sendo utilizados

--- WebServer/src/models/usuario/create.js
+++ WebServer/src/models/usuario/create.js
@@ -3,11 +3,11 @@ import { log } from "../../utils/log.js";
 
 export async function modelsUsuarioCreate({ nome, email, senha, cargo, tipo_usuario }) {
     try {
-        let instrucao = `insert into usuario(nome, email, senha, cargo, tipo_usuario) VALUES ("${nome}","${email}","${senha}","${cargo},${tipo_usuario}");`
+        let instrucao = `insert into usuario(nome, email, senha, cargo, tipo_usuario) VALUES ("${nome}","${email}","${senha}","${cargo}","${tipo_usuario}");`
         return await executar(instrucao)
     } catch (e) {
         log(import.meta.url, e)
-        throw " ERRO NO BANCO "
+        throw " ERRO NO BANCO (MODELS) "
     }
 
 }
--- WebServer/src/routes/usuarios/index.js
+++ WebServer/src/routes/usuarios/index.js
@@ -1,17 +1,10 @@
 import express from "express"
-//import controllersUsuarioRead from "../../controllers/usuarios/read.js"
+
 import { controllersUsuarioCreate } from "../../controllers/usuarios/create.js"
 import { controllersUsuarioUpdate } from "../../controllers/usuarios/update.js"
-import { controllersUsuarioAuthCreate } from "../../controllers/usuarios/auth/auth.js"
-
-//import controllersUsuarioDelete from "../../controllers/usuarios/delete.js"
 
 const routerUsuarioIndex = express.Router();
-//routerUsuarioIndex.get("/", controllersUsuarioRead)
-routerUsuarioIndex.post("/auth", controllersUsuarioAuthCreate)
-routerUsuarioIndex.post("/", controllersUsuarioCreate)
-routerUsuarioIndex.put("/", controllersUsuarioUpdate)
-//routerUsuarioIndex.delete("/", controllersUsuarioDelete)
-
+routerUsuarioIndex.post("/create", controllersUsuarioCreate)
+routerUsuarioIndex.put("/update", controllersUsuarioUpdate)
 
 export default routerUsuarioIndex
