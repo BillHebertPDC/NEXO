0b84f9d1151773e069badadd7c2ac516c3d427ab
richh-brreto <richard.barreto@sptech.school>
2025-09-08 10:51:43 -0300

    fix: (WebDataViz) Auth - padronizado o nome de conexão das rotas - services com a função correta

--- WebServer/src/services/usuario/auth/auth.js
+++ WebServer/src/services/usuario/auth/auth.js
@@ -1,28 +1,17 @@
 
-import 'dotenv/config';
+import { modelsUsuarioAuthAutenticar } from "../../../models/usuario/auth/auth.js";
+import { log } from "../../../utils/log.js";
 
 export const servicesUsuarioAuthAutenticar = async ({ email, senha }) => {
-    /*
     try {
-        let user = await modelsUsuarioAuthAutenticar({ email: email })
-        if (user.length > 0) {
-            const valid = await bcrypt.compare(senha, user[0].senha)
-            if (valid) {
-                let interesse = await modelsUsuarioAuthInteresse({ interesse: user[0].interesse })
-                let max = await modelsQuizzMax({ id: user[0].id })
-                const token = jwt.sign(
-                    { id: user[0].id, email: user[0].email },
-                    process.env.JWT_SECRET,
-                    { expiresIn: "24h" }
-                )
-                delete user[0].senha;
-                return { login: true, token: token, user: user[0], interesse: interesse, max: max }
-            }
+        let user = await modelsUsuarioAuthAutenticar({ email: email });
+        if (user.length > 0 && user[0].senha === senha) {
+            delete user[0].senha;
+            return { login: true, user: user[0] };
         }
-        return { login: false }
+        return { login: false };
     } catch (e) {
-        log(import.meta.url, e)
-        throw e + " ERROR SERVICES "
+        log(import.meta.url, e);
+        throw e + " ERROR SERVICES ";
     }
-    */
-}
+};
