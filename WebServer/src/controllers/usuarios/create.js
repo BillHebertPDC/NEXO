9bb2fd475bebe86d73909f1802e0a921f1239899
BillHebertPDC <bill.choi@sptech.school.com>
2025-09-09 10:03:17 -0300

    fix (controllers/usuarios/create): modificação nos inputs

--- WebServer/src/controllers/usuarios/create.js
+++ WebServer/src/controllers/usuarios/create.js
@@ -4,8 +4,8 @@ import { servicesUsuarioCreate } from "../../services/usuario/create.js";
 export const controllersUsuarioCreate = async (req, res, next) => {
     try {
         const PARAMETROS = req.params;
-        const { nome, email, senha, cargo, tipo_usuario } = req.body
-        await servicesUsuarioCreate({ nome: nome, email: email, senha: senha, cargo: cargo, tipo_usuario: tipo_usuario })
+        const { nome, cnpj, email, telefone, senha } = req.body
+        await servicesUsuarioCreate({ nome, cnpj, email, telefone, senha })
         return res.status(200).json({ ok: true });
     } catch (e) {
         log(import.meta.url, e)
