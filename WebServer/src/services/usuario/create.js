f710e7a919f7c69aad1a524fa8313cc24f3b10ea
BillHebertPDC <bill.choi@sptech.school.com>
2025-09-09 10:04:08 -0300

    fix (services/usuarios/create): modificação nos inputs

--- WebServer/src/services/usuario/create.js
+++ WebServer/src/services/usuario/create.js
@@ -1,8 +1,8 @@
 import { modelsUsuarioCreate } from "../../models/usuario/create.js"
 import { log } from "../../utils/log.js"
-export const servicesUsuarioCreate = async ({ nome, email, senha, cargo, tipo_usuario }) => {
+export const servicesUsuarioCreate = async ({ nome, cnpj, email, telefone, senha }) => {
     try {
-        return await modelsUsuarioCreate({ nome: nome, email: email, senha: senha, cargo: cargo, tipo_usuario: tipo_usuario })
+        return await modelsUsuarioCreate({ nome, cnpj, email, telefone, senha })
     } catch (e) {
         log(import.meta.url, e)
         throw e + " ERROR SERVICES "
